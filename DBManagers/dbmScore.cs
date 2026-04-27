fb28dddd59bfe63dfb7e4643b70bb1b23bad7923
CausingWonder <jmrouse04@gmail.com>
2026-04-27 14:20:53 -0400

    added GA#10 cls's

---
+++ DBManagers/dbmScore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameActivity.DBManagers
+{
+    internal class dbmScore
+    {
+    }
+}
---
+++ clsLogic/clsAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameActivity.clsLogic
+{
+    internal class clsAuthenticationService
+    {
+    }
+}
